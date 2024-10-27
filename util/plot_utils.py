a72c46759992a82a6a7bf52119f9b90a7501cdf3
Duc Quan <leducquan16112002@gmail.com>
2024-10-27 16:23:55 +0700

    fix

--- util/plot_utils.py
+++ util/plot_utils.py
@@ -62,7 +62,7 @@ def plot_logs(logs, fields=('class_error', 'loss_bbox_unscaled', 'mAP'), ewm_col
                 ).ewm(com=ewm_col).mean()
                 axs[j].plot(coco_eval, c=color)
             else:
-                df.interpolate().ewm(com=ewm_col).mean().plot(
+                df.drop('test_coco_eval_bbox', axis=1).interpolate().ewm(com=ewm_col).mean().plot(
                     y=[f'train_{field}', f'test_{field}'],
                     ax=axs[j],
                     color=[color] * 2,
