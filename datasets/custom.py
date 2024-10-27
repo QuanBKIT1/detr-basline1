f50ed3a1b0409f2bf6caac0523e4df580ed58cdf
Duc Quan <leducquan16112002@gmail.com>
2024-10-27 08:46:59 +0700

    change

--- datasets/custom.py
+++ datasets/custom.py
@@ -10,11 +10,11 @@ from .coco import CocoDetection, make_coco_transforms
 def build(image_set, args):
     root = Path(args.coco_path)
     assert root.exists(), f'provided path {root} to custom dataset does not exist'
-    training_json_file = 'custom_train.json'
-    validation_json_file = 'custom_val.json'
+    training_json_file = 'instances_train.json'
+    validation_json_file = 'instances_val.json'
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / training_json_file),
-        "val": (root / "val2017", root / "annotations" / validation_json_file),
+        "train": (root / "train", root / "annotations" / training_json_file),
+        "val": (root / "val", root / "annotations" / validation_json_file),
     }
 
     img_folder, ann_file = PATHS[image_set]
